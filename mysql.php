2f9f07ab5a1899c39c76556a0873c4ae0a9765ea
Chris Rasch <crasch@RFX-5-248.reelfx.com>
2016-09-06 16:32:54 -0500

    fixed reservation system

--- mysql.php
+++ mysql.php
@@ -1,8 +1,8 @@
 <?php
-    $host_name  = "db645677917.db.1and1.com";
-    $database   = "db645677917";
-    $user_name  = "dbo645677917";
-    $password   = "Anabelsuz83!";
+    $host_name  = "db645906944.db.1and1.com";
+    $database   = "db645906944";
+    $user_name  = "dbo645906944";
+    $password   = "Jbuffett83!";
 
     
     $connect = mysqli_connect($host_name, $user_name, $password, $database);
@@ -12,19 +12,28 @@
       die("'Connection Failed!'.mysqli_connect_error().");
     }
 
-    if (array_key_exists('email', $_POST) OR array_key_exists('password', $_POST)) {
-        
-        $query = "SELECT `id` FROM `users` WHERE email = '".mysqli_real_escape_string($connect, $_POST['email'])."'";
+    
+
+    if (array_key_exists('arrive', $_POST) OR array_key_exists('depart', $_POST)) {
+
+        $query = "SELECT `id` FROM `reserve` WHERE `checkOut` > date('".mysqli_escape_string($connect, $_POST['arrive'])."')";
+
         $result = mysqli_query($connect,$query);
 
+
         if (mysqli_num_rows($result) > 0) {
-            echo "That email has already been taken!";
-        } else {
-            $query = "INSERT INTO `users` (`email`,`password`) VALUES('".mysqli_real_escape_string($connect, $_POST['email'])."', '".mysqli_real_escape_string($connect, $_POST['password'])."')";
+            echo "That date has already been taken!";
+        } 
+
+        else {
+
+            $query = "INSERT INTO `reserve` (`checkIn`,`checkOut`) VALUES('".mysqli_real_escape_string($connect, $_POST['arrive'])."', '".mysqli_real_escape_string($connect, $_POST['depart'])."')";
 
             if(mysqli_query($connect,$query)) {
-                echo "Thanks For Registering!";
-            } else {
+                echo "Thanks For Your Reservation!  We Hope You Enjoy Your Stay!";
+            } 
+
+            else {
                 echo "Please Try Again!";
             }
             
@@ -32,14 +41,6 @@
 
     }
     
-  
-
-    //Add new query
-    //$query = "INSERT INTO `users` (`email`, `password`) VALUES('anabelelise@gmail.com','ilovemydad')";
-
-    //Update query
-    //$query = "UPDATE `users` SET password = '6ER23IO' WHERE email = 'anotherEmail83@gmail.com' LIMIT 1";
-
     
 ?>
 
@@ -47,12 +48,33 @@
 <html>
 <head>
     <title>Test Database</title>
+    <script   src="https://code.jquery.com/jquery-3.1.0.min.js"   integrity="sha256-cCueBR6CsyA4/9szpPfrX3s49M9vUU5BgtiJj06wt/s="   crossorigin="anonymous"></script>
+    <script type="text/javascript" src="datedropper.js"></script>
+    <script type="text/javascript" src="flatpickr-gh-pages/dist/flatpickr.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="flatpickr-gh-pages/dist/flatpickr.min.css">
+    <link rel="stylesheet" type="text/css" href="datedropper.css">
 </head>
 <body>
 <form method="post">
-<h3>Email: <input name="email" type="text" placeholder="Enter Email" required></h3>
-<h3>Password: <input name="password" type="password" placeholder="Enter Password" required></h3>
-<input type="submit" name="Submit">
+    <input id="arrive" name="arrive" type="text" placeholder="Arrive" required>
+    <input id="depart" name="depart" type="text" placeholder="Depart" required>
+    <input type="submit" name="Search" value="Search">
 </form>
+<script type="text/javascript">
+    var check_in = $("#arrive").flatpickr({
+        altInput: true,
+        altFormat: "Y-m-d",
+        minDate: new Date()
+    });
+
+    var check_out = $("#depart").flatpickr({
+        altInput: true,
+        altFormat: "Y-m-d",
+        minDate: new Date()
+    });
+
+    check_in.config.onChange = dateObj => check_out.set("minDate", dateObj.fp_incr(1));
+    check_out.config.onChange = dateObj => check_in.set("maxDate", dateObj.fp_incr(-1));
+</script>
 </body>
 </html>
