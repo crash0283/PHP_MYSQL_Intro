9892593395fb841ea6092b651a8d298ff9da340a
Chris Rasch <crasch@RFX-5-248.reelfx.com>
2016-09-07 12:49:55 -0500

    added bootstrap and css file

--- mysql.php
+++ mysql.php
@@ -6,6 +6,8 @@
 
 
     $connect = mysqli_connect($host_name, $user_name, $password, $database);
+    $alert = '';
+    $error = '';
 
     if(mysqli_connect_errno())
     {
@@ -17,7 +19,7 @@
     if (array_key_exists('arrive', $_POST) OR array_key_exists('depart', $_POST)) {
 
         if ($_POST['arrive'] == '' || $_POST['depart'] == '') {
-            echo "Please Choose Reservation Dates!";
+            $error = "Please Choose Reservation Dates!";
         } else {
 
 
@@ -29,7 +31,7 @@
 
 
         if (mysqli_num_rows($result) > 0) {
-            echo "That date has already been taken!";
+            $error = "That date has already been taken!";
         }
 
         else {
@@ -37,11 +39,11 @@
             $query = "INSERT INTO `reserve` (`checkIn`,`checkOut`) VALUES('".mysqli_real_escape_string($connect, $_POST['arrive'])."', '".mysqli_real_escape_string($connect, $_POST['depart'])."')";
 
             if(mysqli_query($connect,$query)) {
-                echo "Thanks For Your Reservation!  We Hope You Enjoy Your Stay!";
+                $alert = "Thanks For Your Reservation!  We Hope You Enjoy Your Stay!";
             }
 
             else {
-                echo "Please Try Again!";
+                $error =  "Please Try Again!";
             }
 
         }
@@ -58,18 +60,43 @@
     <head>
         <title>Test Database</title>
         <script src="https://code.jquery.com/jquery-3.1.0.min.js" integrity="sha256-cCueBR6CsyA4/9szpPfrX3s49M9vUU5BgtiJj06wt/s=" crossorigin="anonymous"></script>
+        <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
         <script type="text/javascript" src="datedropper.js"></script>
         <script type="text/javascript" src="flatpickr-gh-pages/dist/flatpickr.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
         <link rel="stylesheet" type="text/css" href="flatpickr-gh-pages/dist/flatpickr.material_blue.min.css">
         <link rel="stylesheet" type="text/css" href="datedropper.css">
+        <link rel="stylesheet" type="text/css" href="mysql.css">
     </head>
 
     <body>
-        <form method="post">
-            <input id="arrive" name="arrive" type="text" placeholder="Arrive" required>
-            <input id="depart" name="depart" type="text" placeholder="Depart" required>
-            <input type="submit" name="Search" value="Search">
-        </form>
+        <div class="container">
+            <div class="row">
+                <form class="form-inline" method="post">
+                    <div class="form-group">
+                        <div class="input-group">
+                            <input class="form-control" id="arrive" name="arrive" type="text" placeholder="Arrive">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="input-group">
+                            <input class="form-control" id="depart" name="depart" type="text" placeholder="Depart">
+                        </div>
+                    </div>
+                    <input class="btn btn-primary" type="submit" name="Search" value="Search Reservations">
+                </form>
+                <div class="output"><?php 
+                    if($alert) {
+                        echo '<div class="alert alert-success" role="alert">'.$alert.'</div>';
+                    } 
+                    else if($error) {
+                        echo '<div class="alert alert-danger" role="alert">'.$error.'</div>';
+                        }?>
+                            
+                </div>
+            </div>
+        </div>
         <script type="text/javascript">
             var check_in = $("#arrive").flatpickr({
                 altInput: true,
